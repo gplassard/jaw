1b39b96bde616482a860cdcbb31a7836ab50f890
Gabriel Plassard <gabriel.plassard@gmail.com>
2024-02-03 01:43:41 +0100

    migrate to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,17 +21,17 @@ application {
 }
 
 dependencies {
-    implementation("info.picocli:picocli:4.7.5")
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation("software.amazon.awssdk:cloudwatchlogs:2.23.16")
-    implementation("software.amazon.awssdk:sts:2.23.16")
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.10.0")
-    testImplementation("org.assertj:assertj-core:3.25.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+    implementation(libs.picocli)
+    implementation(libs.commons.lang)
+    implementation(libs.aws.cloudwatch)
+    implementation(libs.aws.sts)
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    implementation(libs.logback)
+    testImplementation(libs.junit.api)
+    testRuntimeOnly(libs.junit.engine)
+    testImplementation(libs.mockito)
+    testImplementation(libs.assertj)
 }
 
 tasks.withType<Test> {
